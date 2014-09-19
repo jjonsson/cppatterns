416d945cca1e4463137e8f7ccb8c7efc5a74cb49
James Jonsson <jay_jonsson@hmc.edu>
2014-09-19 00:18:39 -0700

    improved tests

---
+++ test.cpp
@@ -0,0 +1,92 @@
+// Author: James Jonsson
+// Date: 9/18/14
+//
+// This file contains automated tests for pattern_matching.h
+// Compiled successfully under Clang 3.4
+
+#include <string>
+#include <iostream>
+#include <list>
+#include <functional>
+#include <type_traits>
+#include "pattern_matching.h"
+
+using namespace std;
+
+// Keep the tests in a list for easy access and scaling.
+list<function<bool()> > tests =
+{
+  []{  // Test 1: integer matching
+    MATCH(42)
+      CASE(0) return false;
+      CASE(42) return true;
+    ENDMATCH
+      return false;
+  },
+  []{  // Test 2: string matching
+    MATCH("foo")
+      CASE("bar") return false;
+      CASE("baz") return false;
+      CASE("foo") return true;
+    ENDMATCH
+    return false;
+  },
+  []{  // Test 3: multi-line blocks
+    MATCH(12)
+      CASE(11) {
+        bool b = false;
+        return b;
+      }
+      CASE(12) {
+        bool b = true;
+        return b;
+      }
+      CASE(13) return false;
+    ENDMATCH
+    return false;
+  },
+  []{  // Test 4: binding values to pattern variables
+    char c = 'z';
+    MATCH('j')
+      CASE('a') return false;
+      CASE(c) return c == 'j';
+      CASE('j') return false;  // it shouldn't pass the second case.
+    ENDMATCH
+    return false;
+  }
+};
+
+int main() {
+  
+  list<int> results;
+  
+  // Run the tests and save the results.
+  int test_number = 1;
+  for (auto& test : tests) {
+    if (!test()) {
+      results.push_back(test_number);
+    }
+    ++test_number;
+  }
+  
+  if (results.empty()) {
+    cout << "All tests passed!" << endl;
+  }
+  else {  // Print out which tests failed.
+    string line = "Test";
+    
+    // Make "Test" plural if necessary.
+    if (results.size() > 1) {
+      line.push_back('s');
+    }
+    
+    for (auto result : results) {
+      line.append(" " + to_string(result) + ",");
+    }
+    
+    // There's an extra comma at the end.
+    cout << line.substr(0, line.size() - 1) << " failed." << endl;
+  }
+  
+  return 0;
+}
