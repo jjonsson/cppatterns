d8be1694dd9d193d44357061e18af42ed749a340
James Jonsson <jay_jonsson@hmc.edu>
2014-09-26 04:33:10 -0700

    added a check that the types match

--- match_private.hpp
+++ match_private.hpp
@@ -7,10 +7,14 @@
 #ifdef CPM_MATCH_PRIVATE_HPP
 #define CPM_MATCH_PRIVATE_HPP
 
+#include <type_traits>
+
 namespace cpm {
   template<class T, class U>
   std::function<void(case_list_t<U>)> match(const T& scrutinee);
   {
+    static_assert(std::is_convertible_to<T, U>,
+      "The type of this pattern does not match the type of the value to be matched.");
     return [](case_list_t<U> cases) {
       for (auto& c : cases) {
         bool result = c.match_against(scrutinee);
